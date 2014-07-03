c984b2943af85f9cb11ea5f7de2d1d31568ac766
Emmanuel <m4nu@hotmail.fr>
2014-07-03 15:56:01 +0800

    Adds base grid module

---
+++ public/scripts/src/grid.js
@@ -0,0 +1,18 @@
+/**
+ * Grid Object
+ * Responsible for creating the grid from a configuration object
+ */
+
+// Configuration Object
+var conf = {
+    "size": 4,
+    "colors": [ "DD232D", "E0DC2E", "46E62D", "37E4B7", "3079E0", "5A1AE0", "FB18D6", "FB421B" ]
+};
+
+var grid = (function() {
+    return {
+        //
+    }
+})();
+
+module.exports = grid;
