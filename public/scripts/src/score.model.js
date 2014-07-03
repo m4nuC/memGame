cda0e16eb86699a291c5e40e7586a2f63badb72f
Emmanuel <m4nu@hotmail.fr>
2014-07-03 15:32:36 +0800

    Adds base score model

---
+++ public/scripts/src/score.model.js
@@ -0,0 +1,31 @@
+/**
+ * Score Object
+ * Responsible for fetching high scores and syncing them to server
+ * Get injected with the DOM manipulation library
+ */
+
+var scoreModel = (function(DOMlib) {
+    return {
+        // Stores current
+        currentScore: null,
+
+        // Stores the current high scores using "playerName:value" format
+        highScore: {
+        },
+
+        fetch: function() {
+            DOMlib.getJSON( _GLOBALS.baseURL + "/scores")
+                .then( this._setHighScores );
+        },
+
+        save: function() {
+            //
+        },
+
+        _setHighScores: function( data ) {
+            this.highScore = data
+        }
+    }
+})( $ );
+
+module.exports = scoreModel;
