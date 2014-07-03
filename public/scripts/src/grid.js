1e91842a9c38dc7bf4138a55eae3be62bac58b5d
Emmanuel <m4nu@hotmail.fr>
2014-07-03 16:46:06 +0800

    Adds grid generation logic

--- public/scripts/src/grid.js
+++ public/scripts/src/grid.js
@@ -4,14 +4,45 @@
  */
 
 // Configuration Object
-var conf = {
+var config = {
     "size": 4,
     "colors": [ "DD232D", "E0DC2E", "46E62D", "37E4B7", "3079E0", "5A1AE0", "FB18D6", "FB421B" ]
 };
 
+var cell = require('./cell.js');
+
 var grid = (function() {
+
+    _getShuffeledColors = function( colors ) {
+        var shuffeledColors = [];
+        colors = colors.concat( colors.slice() );
+        var length = colors.length;
+        do {
+            var rand = Math.floor(Math.random() *  length);
+            shuffeledColors.push(colors.splice(rand, 1)[0]);
+            length = colors.length;
+        }
+        while ( length!= 0 )
+        return shuffeledColors;
+    };
+
+    _generateGrid = function() {
+        var colors = _getShuffeledColors( config.colors );
+        var el = document.createElement( 'div' );
+        for (var i = 0, lgth = colors.length ; i < lgth ; i++) {
+            var currCell = cell.create( colors[i]);
+            el.appendChild( currCell );
+        };
+        return el;
+    };
+
     return {
-        //
+        init: function() {
+            _generateGrid();
+            console.log( );
+            //document.getElementById( 'game-board' )
+             //       .appendChild( _generateGrid() );
+        }
     }
 })();
 
